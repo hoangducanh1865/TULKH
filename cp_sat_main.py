fe019f05ca18ca9da986b0b2ec0e1bec81ad28a4
hoangducanh1865 <hoangducanh.1865@gmail.com>
2026-07-11 17:05:29 +0700

    feat: update

---
+++ cp_sat_main.py
@@ -0,0 +1,33 @@
+from ortools.sat.python import cp_model
+
+def main():
+    pass
+    # Input
+
+    # Model & Solver
+    model = cp_model.CpModel()
+    solver = cp_model.CpSolver()
+    if not model or not solver:
+        print("MODEL/SOLVER NOT OK")
+        return
+
+    # Bien
+
+    # Rang buoc
+
+    # Ham muc tieu
+    obj = ...
+    model.Minimize(obj) # ["Minimize", "Maximize"]
+
+    # Solve
+    status = solver.Solve(model)
+
+    # Output
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print(int(solver.ObjectiveValue()))
+        # print(int(solver.Value(x)))
+    else:
+        print("-1")
+
+if __name__ == "__main__":
+    main()
---
+++ scip_glop_main.py
@@ -0,0 +1,32 @@
+from ortools.linear_solver import pywraplp
+
+def main():
+    pass
+    # Input
+    
+    # Solver
+    solver = pywraplp.Solver.CreateSolver("SCIP") # ["SCIP", "GLOP"]
+    if not solver:
+        print("SOLVER NOT OK")
+        return
+    
+    # Bien
+    
+    # Rang buoc
+    
+    # Ham muc tieu
+    obj = ...
+    solver.Minimize(obj) # ["Minimize", "Maximize"]
+    
+    # Solve
+    status = solver.Solve()
+    
+    # Output
+    if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
+        print(int(solver.Objective().Value()))
+        # print(int(x.solution_value()))
+    else:
+        print("-1")
+    
+if __name__ == "__main__":
+    main()
